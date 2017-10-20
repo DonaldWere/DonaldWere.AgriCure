13809d0f9fdf3e8dfdc6c72497d6dd3e08dc954c
Donald Were <donwere7@gmail.com>
2017-10-20 07:32:08 +0300

    Add files via upload

---
+++ includes/db_connect.php
@@ -0,0 +1,3 @@
+<?php
+$con=mysqli_connect('localhost','root','','crop') or die('error with the connection');
+?>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<div class="page-header"  style="margin-top:0px; padding:0px;">
+<nav class="navbar navbar-inverse" style="background-color:#0170AF; border:none;">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <p><h1><b>AgriCure Limited</b></h1></p>
+    </div>
+  </div>
+</nav>
+</nav>
+</div>
